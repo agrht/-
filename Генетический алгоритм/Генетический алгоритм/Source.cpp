bb4a94535575a83b93fb2e56704e70982687fd39
agrht <ru052@bk.ru>
2020-11-11 21:23:54 +0300

    unreal but it works

--- Генетический алгоритм/Генетический алгоритм/Source.cpp
+++ Генетический алгоритм/Генетический алгоритм/Source.cpp
@@ -6,8 +6,9 @@
 #include <iostream>
 #include <cmath>
 #include <ctime>
-//#include "Vector.h"
 #include <valarray>
+#include <ctime>
+#define PI 3.14159265358979323846
 
 using namespace std;
 
@@ -23,13 +24,45 @@ double rosenbrock_func(valarray<double> arg)
 		throw - 1;
 }
 
-double j_func(valarray<double> arg)
+double icicle_func(valarray<double> arg)
 {
 	if (arg.size() == 2)
 	{
 		double x = arg[0];
 		double y = arg[1];
 		return (1+sin(10*x)+cos(2*x)+cos(2*x+2*y)+cos(2*y)+sin(20*y)+y*y);
+	}
+	else
+		throw - 1;
+}
+
+std::valarray<std::valarray<double>> A_matrix = { {-0.940, -0.536, -0.743}, {-0.502,  0.804,  0.769}, {-0.428, -0.789,  0.204} };
+std::valarray<std::valarray<double>> B_matrix = { { 0.590,  0.160, -0.681}, { 0.387,  0.945, -0.195}, {-0.231,  0.152,  0.295} };
+std::valarray<std::valarray<double>> C_matrix = { {-0.896, -0.613, -0.463}, { 0.038, -0.428, -0.714}, { 0.103,  0.741, -0.317} };
+std::valarray<std::valarray<double>> D_matrix = { {-0.754, -0.558, -0.989}, {-0.702,  0.881,  0.397}, {-0.056,  0.085, -0.616} };
+
+double horrific_func(std::valarray<double> argument) 
+{
+	if (argument.size() == 2)
+	{
+		double x = argument[0];
+		double y = argument[1];
+		double result = 0;
+		double sum1 = 0, sum2 = 0;
+		double A, B, C, D;
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				A = A_matrix[i][j];
+				B = B_matrix[i][j];
+				C = C_matrix[i][j];
+				D = D_matrix[i][j];
+				sum1 += A * sin(i * PI * (x - 1 / 2)) * sin(j * PI * (y - 1 / 2)) + B * cos(i * PI * (x - 1 / 2)) * cos(j * PI * (y - 1 / 2));
+				sum2 += C * sin(i * PI * (x - 1 / 2)) * sin(j * PI * (y - 1 / 2)) + D * cos(i * PI * (x - 1 / 2)) * cos(j * PI * (y - 1 / 2));
+			}
+		}
+		return sqrt(sum1 * sum1 + sum2 * sum2);
 	}
 	else
 		throw - 1;
@@ -44,17 +77,9 @@ valarray<double> mutation(const valarray<double> &x0,const valarray<double> &x1)
 	return child;
 }
 
-double mutation_double(double x0, double x1)  // мутация: генерация случайной величины
-{
-	double child;
-	const int NUM = 100000000;
-	child = fabs((double)((NUM * rand()) % (int)((x1 - x0) * NUM) + 1) / NUM) + x0;	
-	return child;
-}
-
 valarray<double> inversion(const valarray<double> &x, double eps)  // инверсия: поиск в окрестностях точки
 {
-	valarray<double> x_invers;
+	valarray<double> x_invers(2);
 	static int sign = 0;
 	sign++;
 	sign %= 2;
@@ -90,7 +115,7 @@ void crossover_population(valarray<double> *population, double eps,const valarra
 {
 	for (int i = 0; i < 2; i++)
 	{
-		crossover(population, eps, x0[i], x1[i]);
+		crossover(population, eps, x0, x1);
 	}
 }
 
@@ -117,30 +142,40 @@ valarray<double> genetic(const valarray<double> &x0,const valarray<double> &x1, 
 	for (int i = 0; i < population_size; i++)   // Формирование начальной популяции
 	{
 		population[i] = mutation(x0, x1);
-		f[i] = rosenbrock_func(population[i]);
+		f[i] = horrific_func(population[i]); //менять тут
 	}
-	sort(population, f);
-	do {
+	sort(population, f); //?
+	do 
+	{
 		iter++;
 		crossover_population(population, eps, x0, x1);
 		for (int i = 0; i < population_size; i++)
-			f[i] = rosenbrock_func(population[i]);
+			f[i] = horrific_func(population[i]); //менять тут
 		sort(population, f);
-	} while (fabs(f[0]) > eps && iter < 200000);
+	} while (fabs(f[0]) > eps && iter < 20000);
 	cout << iter << " iterations" << endl;
 	return population[0];
 }
 int main()
 {
+	unsigned int start_time = clock(); //начальное время
 	srand(time(NULL));
 	valarray<double> individ_1(2);	
 	valarray<double> individ_2(2);
+	valarray<double> itog(2);
 	for (int i = 0; i < 2; i++)
 	{
-		individ_1[i] = 3+i;
-		individ_2[i] = 2+i;
+		individ_1[i] = i;//3+i;
+		individ_2[i] = i + 1;//2+i;
 	}
-	cout << genetic(individ_1, individ_2, 0.000001);
+	itog = genetic(individ_1, individ_2, 0.000001);
+	for (int i = 0; i < 2; i++)
+	{
+		std::cout << itog[i] << endl;;
+	}
+	unsigned int end_time = clock();
+	unsigned int search_time = end_time - start_time;
+	std::cout << " Time  " << search_time / 1000 << "  sec";
 	cin.get();
 	return 0;
 }
